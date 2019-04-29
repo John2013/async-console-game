cd1d407b87046322948f7af1c8ae83356dfc168a
Evgeny Timofeyev <nawasington@ya.ru>
2019-04-29 17:43:53 +0300

    Добавлена мерцающая звезда

--- main.py
+++ main.py
@@ -5,9 +5,20 @@ import curses
 def draw(canvas):
     row, column = (5, 20)
     canvas.border()
-    canvas.addstr(row, column, 'Hello, World!')
-    canvas.refresh()
-    time.sleep(1)
+    curses.curs_set(0)
+    while True:
+        canvas.addstr(row, column, '*', curses.A_DIM)
+        canvas.refresh()
+        time.sleep(2)
+        canvas.addstr(row, column, '*')
+        canvas.refresh()
+        time.sleep(.3)
+        canvas.addstr(row, column, '*', curses.A_BOLD)
+        canvas.refresh()
+        time.sleep(.5)
+        canvas.addstr(row, column, '*')
+        canvas.refresh()
+        time.sleep(.3)
 
 
 if __name__ == '__main__':
