d491eee819711f2e90e9684d594c9658751390ba
Evgeny Timofeyev <nawasington@ya.ru>
2019-06-27 13:19:09 +0300

    шаг 3 синхронная мигающая звезда

--- main.py
+++ main.py
@@ -4,10 +4,24 @@ import curses
 
 def draw(canvas):
     row, column = (5, 20)
+    curses.curs_set(False)
     canvas.border()
     canvas.addstr(row, column, 'Hello, World!')
     canvas.refresh()
-    time.sleep(1)
+    while True:
+        row, column = (3, 7)
+        canvas.addstr(row, column, '*', curses.A_DIM)
+        canvas.refresh()
+        time.sleep(2.)
+        canvas.addstr(row, column, '*', curses.A_NORMAL)
+        canvas.refresh()
+        time.sleep(.3)
+        canvas.addstr(row, column, '*', curses.A_BOLD)
+        canvas.refresh()
+        time.sleep(.5)
+        canvas.addstr(row, column, '*', curses.A_NORMAL)
+        canvas.refresh()
+        time.sleep(.3)
 
 
 if __name__ == '__main__':
