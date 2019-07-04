6d249ee360a8644dcfe00c6ad8b8eee78c21b3a7
Evgeny Timofeyev <nawasington@ya.ru>
2019-07-04 14:35:14 +0300

    больше мусора богу мусора

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from typing import List, Coroutine
 
 from curses_tools import draw_frame, read_controls, get_frame_size
 
+coroutines: List[Coroutine] = []
+
 
 def run_coroutines(coroutines: List[Coroutine], canvas, tic_timeout=0.):
     while True:
@@ -19,6 +21,15 @@ def run_coroutines(coroutines: List[Coroutine], canvas, tic_timeout=0.):
             break
         canvas.refresh()
         time.sleep(tic_timeout)
+
+
+def get_playground_border(canvas):
+    min_row, min_col = (1, 1)
+    max_row, max_col = canvas.getmaxyx()
+    canvas_padding = 2
+    max_row -= canvas_padding
+    max_col -= canvas_padding
+    return min_row, min_col, max_row, max_col
 
 
 async def blink(canvas, row, column, symbol='*', start_step=0):
@@ -61,13 +72,11 @@ async def fire(canvas, start_row, start_column, rows_speed=-0.3, columns_speed=0
 
     symbol = '-' if columns_speed else '|'
 
-    rows, columns = canvas.getmaxyx()
-    max_row, max_column = rows - 2, columns - 2
-    min_row, min_col = 1, 1
+    min_row, min_col, max_row, max_col = get_playground_border(canvas)
 
     curses.beep()
 
-    while min_row < row < max_row and min_col < column < max_column:
+    while min_row < row < max_row and min_col < column < max_col:
         canvas.addstr(round(row), round(column), symbol)
         await asyncio.sleep(0)
         canvas.addstr(round(row), round(column), ' ')
@@ -76,11 +85,7 @@ async def fire(canvas, start_row, start_column, rows_speed=-0.3, columns_speed=0
 
 
 async def ship(canvas, row, column, frames):
-    min_row, min_col = (1, 1)
-    max_row, max_col = canvas.getmaxyx()
-    canvas_padding = 1
-    max_row -= canvas_padding
-    max_col -= canvas_padding
+    min_row, min_col, max_row, max_col = get_playground_border(canvas)
     while True:
         for frame in frames:
             rows_direction, columns_direction, space_pressed = read_controls(canvas)
@@ -110,18 +115,32 @@ async def fly_garbage(canvas, column, garbage_frame, speed=0.5):
     Animate garbage, flying from top to bottom.
     Column position will stay same, as specified on start.
     """
-    rows_number, columns_number = canvas.getmaxyx()
+    min_row, min_col, max_row, max_col = get_playground_border(canvas)
 
-    column = max(column, 0)
-    column = min(column, columns_number - 1)
+    column = max(column, min_col)
+    column = min(column, max_col)
 
-    row = 0
+    row = min_row
 
-    while row < rows_number:
+    while row < max_row:
         draw_frame(canvas, row, column, garbage_frame)
         await asyncio.sleep(0)
         draw_frame(canvas, row, column, garbage_frame, negative=True)
         row += speed
+
+
+async def fill_orbit_with_garbage(canvas, tens_milliseconds_timeout: int=5):
+    garbage_frames = get_frames('./garbage')
+
+    min_row, min_col, max_row, max_col = get_playground_border(canvas)
+
+    while True:
+        column = randint(min_col, max_col)
+        frame = choice(garbage_frames)
+
+        coroutines.append(fly_garbage(canvas, column, frame))
+        for _ in range(tens_milliseconds_timeout):
+            await asyncio.sleep(0)
 
 
 def get_frames(path):
@@ -145,15 +164,11 @@ def draw(canvas):
     canvas.border()
     canvas.nodelay(True)
     canvas.refresh()
-    min_row, min_col = (1, 1)
-    max_row, max_col = canvas.getmaxyx()
-    canvas_padding = 2
-    max_row -= canvas_padding
-    max_col -= canvas_padding
+
+    min_row, min_col, max_row, max_col = get_playground_border(canvas)
 
     symbols = list('+*.:')
 
-    coroutines: List[Coroutine] = []
     stars_coords = []
     stars_count = 200
 
@@ -185,8 +200,7 @@ def draw(canvas):
         ship(canvas, center_row - 1, center_col - 2, ship_frames)
     )
 
-    garbage_frames = get_frames('./garbage')
-    coroutines.append(fly_garbage(canvas, 10, garbage_frames[0]))
+    coroutines.append(fill_orbit_with_garbage(canvas))
 
     tic_timeout = .1
     run_coroutines(coroutines, canvas, tic_timeout)
